af97e2d72fd236952dcaa7cd51576de2c1a9bace
guiwitz <guiwitz@gmail.com>
2020-10-01 17:06:13 +0200

    clean-up

--- morphodynamics/windowing.py
+++ morphodynamics/windowing.py
@@ -2,21 +2,14 @@ import math
 
 import numpy as np
 from scipy.ndimage.morphology import distance_transform_edt, binary_fill_holes
-from scipy.ndimage.measurements import center_of_mass
 import matplotlib.pyplot as plt
 from skimage.measure import find_contours
-from skimage.segmentation import find_boundaries
-from skimage.color import label2rgb
-# from skimage.external.tifffile import imread, imsave
-# from Segmentation import segment
-# from ArtifactGeneration import FigureHelper
-
-# plot = FigureHelper(not True)
 
 
 def label_windows(shape, windows):
-    """Create an image where the sampling windows are shown as regions with unique gray levels."""
-    
+    """Create an image where the sampling windows are shown as regions
+    with unique gray levels."""
+
     tiles = np.zeros(shape, dtype=np.uint16)
     n = 1
     for j in range(len(windows)):
@@ -65,36 +58,49 @@ def create_arc_length_image(shape, c, L):
 def create_windows(c_main, origin, J=None, I=None, depth=None, width=None):
     """Create windows based on contour and windowing parameters.
 
-    Note: to define the windows, this function uses pseudo-radial and pseudo-angular
-    coordinates. The pseudo-radial coordinate is based on the distance transform of
-    the rasterized version of the continuous spline that defines the contour of the
-    cell. The pseudo-angular coordinate for layer j is based on the distance transform
-    of the discrete contour of layer j. So there is a bit of an inconsistency between
-    continuous and discrete contours.
+    Note: to define the windows, this function uses pseudo-radial and
+    pseudo-angular coordinates. The pseudo-radial coordinate is based
+    on the distance transform of the rasterized version of the continuous
+    spline that defines the contour of the cell. The pseudo-angular coordinate
+    for layer j is based on the distance transform of the discrete contour of
+    layer j. So there is a bit of an inconsistency between continuous and
+    discrete contours.
 
-    Parameters:
-        c_main: A rasterized version of the contour, as obtained by rasterize_curve.
-        origin: (y, x) coordinates of the origin of the curve.
-        J: Number of window layers.
-        I: Vector of dimension J specifying the number of windows per layer.
-        depth: Desired depth of the windows.
-        width: Desired width of the windows.
+    Parameters
+    ----------
+    c_main: 2d array
+        A rasterized version of the contour, as obtained by rasterize_curve.
+    origin: tuple
+        (y, x) coordinates of the origin of the curve.
+    J: int
+        Number of window layers.
+    I: list of int
+        Vector of dimension J specifying the number of windows per layer.
+    depth: int
+        Desired depth of the windows.
+    width: int
+        Desired width of the windows.
 
-    Returns:
-        If I and J are specified, a list of lists w such that w[j][i] is an array of all pixel positions that belong to window (j, i). If I and J are not specified, they are determined based on the depth and width arguments and the tuple w, J, I is returned.
+    Returns
+    -------
+    w: 3d list
+        w[i][j][0] and w[0][0][1] are 1d arrays representing
+        lists of x,y indices of pixels belonging to window in i'th layer
+        in j'th window
+    J: int
+        number of layers (calculated if not provided as input)
+    I: list of int
+        number of windows per layer (calculated if not provided as input)
+
     """
 
     origin = [origin[1], origin[0]]
-
-    # plt.figure()
-    # plt.imshow(c_main, 'gray', vmin=-1, vmax=1)
-    # plt.plot(origin[1], origin[0], 'or')
 
     # Compute the distance transform of the main contour
     D_main = distance_transform_edt(-1 == c_main)
 
     # Compute the mask corresponding to the main contour
-    mask_main = binary_fill_holes(-1 < c_main)
+    mask_main = binary_fill_holes(-1 < c_main) # Maybe not necessary? Can't we just use the segmented mask here?
 
     # Divide the radial coordinate into J layers with specified depth
     Dmax = np.amax(D_main * mask_main)
@@ -111,18 +117,14 @@ def create_windows(c_main, origin, J=None, I=None, depth=None, width=None):
     for j in range(J):
         w.append([])
 
-        # The mask containing the interior of the cell starting from the j-th layer
+        # The mask containing the interior of the cell starting from
+        # the j-th layer
         mask = (b[j] <= D_main) * mask_main
 
         # Extract the contour of the mask
-        cvec = np.asarray(find_contours(mask, 0, fully_connected='high')[0], dtype=np.int)
-
-        # Lvec = compute_discrete_arc_length(cvec)
-        # c = create_arc_length_image(mask.shape, cvec, Lvec)
-        # plt.figure()
-        # plt.imshow(c, 'gray', vmin=-Lvec[-1], vmax=Lvec[-1])
-        # plt.plot(origin[1], origin[0], 'or')
-        # # plt.show()
+        cvec = np.asarray(
+            find_contours(mask, 0, fully_connected='high')[0], dtype=np.int
+            )
 
         # Adjust the origin of the contour:
         # on the discrete contour cvec, find the closest point to the origin,
@@ -134,15 +136,11 @@ def create_windows(c_main, origin, J=None, I=None, depth=None, width=None):
         Lvec = compute_discrete_arc_length(cvec)
 
         # Create an image of the contour where the intensity is the arc length
-        c = create_arc_length_image(mask.shape, cvec, Lvec)
-        # plt.figure()
-        # plt.imshow(c, 'gray', vmin=-Lvec[-1], vmax=Lvec[-1])
-        # plt.plot(origin[1], origin[0], 'or')
-        # # plt.show()
+        arc = create_arc_length_image(mask.shape, cvec, Lvec)
 
         # Compute the feature transform of this image:
         # for each pixel position, we get the coordinates of the closest pixel on the contour
-        F = distance_transform_edt(-1 == c, return_distances=False, return_indices=True)
+        F = distance_transform_edt(-1 == arc, return_distances=False, return_indices=True)
 
         # Fill array with arc lengths of closest points on the contour
         #L = np.zeros(c.shape)
@@ -153,15 +151,22 @@ def create_windows(c_main, origin, J=None, I=None, depth=None, width=None):
         # gridx, gridy = np.meshgrid(range(c.shape[1]), range(c.shape[0]))
         # L = c[F[0,:,:][gridy, gridx], F[1,:,:][gridy, gridx]]
 
-        L = c[F[0,:,:], F[1,:,:]]
+        L = arc[F[0, :, :], F[1, :, :]]
 
         # Create sampling windows for the j-th layer
         if compute_num_win:
             I.append(int(math.ceil(Lvec[-1] / width)))
-        s = np.linspace(0, Lvec[-1], I[j] + 1)
+        w_borders = np.linspace(0, Lvec[-1], I[j] + 1)
         for i in range(I[j]):
             # w[-1].append(np.where(mask & (s1[i] <= L) & (L < s1[i+1]) & (b[0] <= D) & (D < b[1])))
-            w[-1].append(np.where(mask & (s[i] <= L) & (L < s[i+1]) & (b[j] <= D_main) & (D_main < b[j+1])))
+            w[-1].append(
+                np.where(
+                    mask & (w_borders[i] <= L) &
+                    (L < w_borders[i+1]) &
+                    (b[j] <= D_main) &
+                    (D_main < b[j+1])
+                )
+            )
             # plt.figure()
             # plt.imshow(w[j][i])
             # plt.show()
@@ -170,66 +175,7 @@ def create_windows(c_main, origin, J=None, I=None, depth=None, width=None):
         # if j == 0:
         #     t = define_contour_positions(Lvec, I[0], cvec, c_main)
 
-    # plt.figure()
-    # plt.imshow(find_boundaries(label_windows(c_main.shape, w)), 'gray')
-    # plt.plot(origin[1], origin[0], 'or')
-    # plt.show()
-
-    # # Artifact generation
-    # plt.figure()
-    # # plt.title('Contour')
-    # plt.imshow(c, 'gray', vmin=-1, vmax=1)
-    # plt.axis('off')
-    # plt.colorbar()
-    # plt.tight_layout()
-    # plt.savefig('Contour.pdf')
-    #
-    # # plt.figure()
-    # # plt.title('Mask')
-    # # plt.imshow(m.astype(np.int), 'gray')
-    #
-    # plt.figure()
-    # # plt.title('Distance transform')
-    # plt.imshow(D, 'gray')
-    # plt.axis('off')
-    # plt.colorbar()
-    # plt.tight_layout()
-    # plt.savefig('Distance transform.pdf')
-    #
-    # # plot.imshow('Contour length', l)
-    #
-    # plt.figure()
-    # # plt.title('Sectors')
-    # plt.imshow(L, 'gray', vmin=0, vmax=1)
-    # plt.axis('off')
-    # plt.colorbar()
-    # plt.tight_layout()
-    # plt.savefig('Sectors.pdf')
-    #
-    # plt.figure()
-    # # plt.title('Windows')
-    # plt.imshow(label2rgb(label_windows(w)))
-    # plt.axis('off')
-    # plt.tight_layout()
-    # plt.savefig('Windows.pdf')
-    #
-    # plt.figure()
-    # # plt.title('Window boundaries')
-    # plt.imshow(find_boundaries(label_windows(w)), 'gray')
-    # plt.axis('off')
-    # plt.tight_layout()
-    # plt.savefig('Window boundaries.pdf')
-
-    # plt.figure()
-    # plt.imshow(label2rgb(label_windows(mask.shape, w)))
-    # plt.figure()
-    # plt.imshow(find_boundaries(label_windows(mask.shape, w)), 'gray')
-    # plt.show()
-
-    if compute_num_win:
-        return w, J, I
-    else:
-        return w
+    return w, J, I
 
 
 def extract_signals_old(y, w):
@@ -248,11 +194,20 @@ def extract_signals_old(y, w):
 def extract_signals(y, w):
     """ Extract the mean and variance of an image over the sampling windows.
 
-    Parameters:
-        y: Image.
-        w: Windows.
+    Parameters
+    ----------
+    y: 2d array, Image
+    w: 3d list
+        list of window indices as output by create_windows()
 
-    Returns: Tuple with mean and variance.
+    Returns
+    -------
+    mean : 2d array
+        mean values of signal in each window. Array of size number of
+        layers times number of windows in outer layer
+    var : 2d array
+        variance values of signal in each window. Array of size number of
+        layers times number of windows in outer layer
     """
 
     # Number of windows
@@ -264,8 +219,8 @@ def extract_signals(y, w):
     # Remember that the number of windows depends on the layer j.
     # Here we use rectangular arrays to store the mean and variance,
     # but some elements will be unused and have a NaN value.
-    mean = np.nan * np.ones((J,Imax))
-    var = np.nan * np.ones((J,Imax))
+    mean = np.nan * np.ones((J, Imax))
+    var = np.nan * np.ones((J, Imax))
 
     # Extraction of the mean and variance
     for j in range(J):
@@ -283,7 +238,14 @@ def show_windows(w, b):
         for i in range(len(w[j])):
             if np.any(w[j][i]):
                 p = [np.mean(w[j][i][0]), np.mean(w[j][i][1])]
-                plt.text(p[1], p[0], str(i), color='yellow', fontsize=4, horizontalalignment='center', verticalalignment='center')
+                plt.text(
+                    p[1],
+                    p[0],
+                    str(i),
+                    color='yellow',
+                    fontsize=4,
+                    horizontalalignment='center',
+                    verticalalignment='center')
 
 
 def calculate_windows_index(w):
