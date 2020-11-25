25cf6a9ba5e9edf85f04fe4797bf76dbf99c6f71
guiwitz <guiwitz@gmail.com>
2020-11-25 01:33:11 +0100

    create missing dataset before tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,13 +3,33 @@ import numpy as np
 from morphodynamics import utils
 from pathlib import Path
 
-data_path = Path('synthetic/data/Results_ilastik/segmented')
+exec(open("tests/create_dataset.py").read())
+
+resultdir = Path('synthetic/data/Results_ilastik')
+
+
+def count_files():
+
+    tracked_files = resultdir.joinpath('segmented').glob('tracked*')
+    assert len(list(tracked_files)) == 40
+
+    raster_files = resultdir.joinpath('segmented').glob('raster*')
+    assert len(list(raster_files)) == 40
+
+    window_image_files = resultdir.joinpath('segmented').glob('window_image*')
+    assert len(list(window_image_files)) == 40
+
+    window_files = resultdir.joinpath('segmented').glob('window_k*')
+    assert len(list(window_files)) == 40
+
+    assert resultdir.joinpath('Results.pkl').is_file()
+    assert resultdir.joinpath('Parameters.yml').is_file()
 
 
 def test_load_rasterized():
     """Check that image is loaded"""
 
-    image = utils.load_rasterized(data_path, 1)
+    image = utils.load_rasterized(resultdir.joinpath('segmented'), 1)
     assert type(image) is np.ndarray
     assert len(image.shape) == 2
 
@@ -17,6 +37,6 @@ def test_load_rasterized():
 def test_load_window_image():
     """Check that image is loaded"""
 
-    image = utils.load_window_image(data_path, 1)
+    image = utils.load_window_image(resultdir.joinpath('segmented'), 1)
     assert type(image) is np.ndarray
     assert len(image.shape) == 2
