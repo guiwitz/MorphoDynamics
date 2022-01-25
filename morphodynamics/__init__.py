989bc8e92baad8a1dd95e10204aa49662b145fb0
guiwitz <guiwitz@users.noreply.github.com>
2022-01-25 11:29:23 +0100

    use separate plugin package

--- morphodynamics/__init__.py
+++ morphodynamics/__init__.py
@@ -5,11 +5,3 @@ try:
 except DistributionNotFound:
     # package is not installed
     pass
-
-from .napari_plugin.napari_gui import MorphoWidget
-from napari_plugin_engine import napari_hook_implementation
-
-@napari_hook_implementation
-def napari_experimental_provide_dock_widget():
-    # you can return either a single widget, or a sequence of widgets
-    return [MorphoWidget]
