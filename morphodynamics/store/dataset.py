19513ebc5f9edcc3c54a45da747505730637f0e3
guiwitz <guiwitz@users.noreply.github.com>
2023-04-27 20:47:39 +0200

    add data object for array/zarr

--- morphodynamics/store/dataset.py
+++ morphodynamics/store/dataset.py
@@ -4,6 +4,7 @@ from microfilm.dataset import TIFFSeries as TIFFSeries_or
 from microfilm.dataset import MultipageTIFF as MultipageTIFF_or
 from microfilm.dataset import ND2 as ND2_or
 from microfilm.dataset import H5 as H5_or
+from microfilm.dataset import Nparray as Nparray_or
 from types import MethodType
 
 
@@ -128,3 +129,42 @@ class H5(H5_or):
         self.signal_name = signal_name
         self.load_frame_morpho = MethodType(load_frame_morpho, self)
         self.load_frame_signal = MethodType(load_frame_signal, self)
+
+class Nparray(Nparray_or):
+    def __init__(
+        self,
+        nparray,
+        expdir,
+        channel_name=None,
+        bad_frames=[],
+        step=1,
+        max_time=None,
+        data_type=None,
+        morpho_name=None,
+        signal_name=None,
+    ):
+        super().__init__(
+            nparray,
+            expdir,
+            channel_name=channel_name,
+            bad_frames=bad_frames,
+            step=step,
+            max_time=max_time,
+            data_type=data_type,
+        )
+        self.morpho_name = morpho_name
+        self.signal_name = signal_name
+        #self.load_frame_morpho = MethodType(load_frame_morpho, self)
+        self.load_frame_signal = MethodType(load_frame_signal, self)
+
+    def load_frame_morpho(self, k):
+        """Load index k of valid frames of the segmentation channel"""
+        
+        if self.morpho_name is not None:
+                time = self.valid_frames[k]
+                image = self.load_frame(self.morpho_name, time)
+                image = image.astype(np.float32)
+                image = (image - image.min()) / (image.max() - image.min())
+                return image
+        else:
+            raise Exception(f"Sorry, no segmentation channel has been provided.")
